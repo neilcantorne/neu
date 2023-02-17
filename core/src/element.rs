5a18f385cc6d19eca56961e07a0f715ebd37bf9d
neilcantorne <cantorn.neil@gmail.com>
2023-02-17 21:39:43 +0800

    implement Element for numerical types

--- core/src/element.rs
+++ core/src/element.rs
@@ -1,3 +1,43 @@
 pub trait Element {
     const CHANNELS: usize;
 }
+
+impl Element for f32 {
+    const CHANNELS: usize = 1;
+}
+
+impl Element for f64 {
+    const CHANNELS: usize = 1;
+}
+
+impl Element for u8 {
+    const CHANNELS: usize = 1;
+}
+
+impl Element for u16 {
+    const CHANNELS: usize = 1;
+}
+
+impl Element for u32 {
+    const CHANNELS: usize = 1;
+}
+
+impl Element for u64 {
+    const CHANNELS: usize = 1;
+}
+
+impl Element for i8 {
+    const CHANNELS: usize = 1;
+}
+
+impl Element for i16 {
+    const CHANNELS: usize = 1;
+}
+
+impl Element for i32 {
+    const CHANNELS: usize = 1;
+}
+
+impl Element for i64 {
+    const CHANNELS: usize = 1;
+}
