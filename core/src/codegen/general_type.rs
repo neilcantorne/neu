32b8bcc0ca583849d0a13d3248a11f9158603279
neilcantorne <cantorn.neil@gmail.com>
2023-02-17 21:47:23 +0800

    add Element and Tensor1

--- core/src/codegen/general_type.rs
+++ core/src/codegen/general_type.rs
@@ -1,7 +1,8 @@
 #[derive(PartialEq, Eq)]
 #[derive(Copy, Clone)]
 pub enum GeneralType {
+    Tensor1(u32, super::Element),
     Tensor2(u32, u32, super::Element),
     Tensor3(u32, u32, u32, super::Element),
-    Scalar(super::ScalarType),
+    Element(super::Element)
 }
