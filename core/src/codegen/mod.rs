a2c307fd222e1a95ddd2160510af727fac83d11d
neilcantorne <cantorn.neil@gmail.com>
2023-02-18 04:28:28 +0800

    update codegen

--- core/src/codegen/mod.rs
+++ core/src/codegen/mod.rs
@@ -2,14 +2,16 @@ mod value;
 mod general_type;
 mod scalar_type;
 mod node;
-mod element;
+mod constant;
+mod element_type;
 mod operand;
 
 pub use value::Value;
 use general_type::GeneralType;
 use scalar_type::ScalarType;
 use node::Node;
-use element::Element;
+use element_type::ElementType;
+use constant::Constant;
 use operand::{
     IntoOperand,
     Operand
