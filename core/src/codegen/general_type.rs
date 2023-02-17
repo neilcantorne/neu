ec03bb599d2acd93f76e0efb08a3d7092cc46738
neilcantorne <cantorn.neil@gmail.com>
2023-02-18 04:28:15 +0800

    update GeneralType

--- core/src/codegen/general_type.rs
+++ core/src/codegen/general_type.rs
@@ -1,8 +1,8 @@
 #[derive(PartialEq, Eq)]
 #[derive(Copy, Clone)]
 pub enum GeneralType {
-    Tensor1(u32, super::Element),
-    Tensor2(u32, u32, super::Element),
-    Tensor3(u32, u32, u32, super::Element),
-    Element(super::Element)
+    Tensor1(u32, super::ElementType),
+    Tensor2(u32, u32, super::ElementType),
+    Tensor3(u32, u32, u32, super::ElementType),
+    Element(super::ElementType)
 }
