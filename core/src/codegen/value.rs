ec7ffe19b2f2ccc5176fc0788c0fb10317381e52
neilcantorne <cantorn.neil@gmail.com>
2023-02-17 18:29:06 +0800

    implement IntoOperand for Value

--- core/src/codegen/value.rs
+++ core/src/codegen/value.rs
@@ -1,3 +1,9 @@
 pub struct Value {
-    pub(super) node: super::Node,
+    pub(super) operand: super::Operand,
+}
+
+impl super::IntoOperand for Value {
+    fn operand(&self) -> super::operand::Operand {
+        return self.operand;
+    }
 }
