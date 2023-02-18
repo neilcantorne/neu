3cfad6e57e0b23251041d42b3fe83d771cc99fe9
neilcantorne <cantorn.neil@gmail.com>
2023-02-18 08:19:10 +0800

    define trainable

--- core/src/layer_trainables.rs
+++ core/src/layer_trainables.rs
@@ -1,3 +1,11 @@
 pub trait LayerTrainables {
-    
+    type Values;
+}
+
+impl<F> LayerTrainables for crate::Tensor<F> {
+    type Values = crate::Value;
+}
+
+impl<F> LayerTrainables for (crate::Tensor<F>, crate::Tensor<F>) {
+    type Values = (crate::Value, crate::Value);
 }
