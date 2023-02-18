085f3fd8d7a82dae00968928330effdf8751928c
neilcantorne <cantorn.neil@gmail.com>
2023-02-18 08:18:57 +0800

    implement Debug for Error

--- core/src/error.rs
+++ core/src/error.rs
@@ -23,3 +23,18 @@ impl From<ErrorVariants> for Error {
         Error(val)
     }
 }
+
+impl std::fmt::Debug for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(match self.0 {
+            ErrorVariants::InvalidTensorLayout => "Invalid tensor layout",
+            ErrorVariants::TensorAllocationFailed => "Tensor allocation failed",
+            ErrorVariants::ElementAllocationFailed => "Element allocation failed",
+            ErrorVariants::DifferentOperandTypes => "Different operand types",
+            ErrorVariants::DifferentOperandDimension => "Different_operand_dimension",
+            ErrorVariants::InvalidOperandTypes => "Invalid operand types",
+            ErrorVariants::IncompatibleOperandDimensions => "Incompatible operand dimensions",
+            ErrorVariants::TensorNonUniformChannel => "Tensor non-uniform channel",
+        })
+    }
+}
