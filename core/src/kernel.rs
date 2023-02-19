b6059f79bef47fe251b5b1bf265d5e6bd44deb0f
neilcantorne <cantorn.neil@gmail.com>
2023-02-20 00:27:10 +0800

    remove kernel

--- core/src/kernel.rs
+++
@@ -1,7 +0,0 @@
-use std::marker::PhantomData;
-
-pub struct Kernel<I: crate::LayerValue, O: crate::LayerValue, T: crate::LayerTrainables> {
-    _input: PhantomData<I>,
-    _output: PhantomData<O>,
-    _trainable: PhantomData<T>,
-}
