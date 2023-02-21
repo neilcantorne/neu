59ff422e9de51f6938aeb8d89132fbfb8fbd7e5e
neilcantorne <cantorn.neil@gmail.com>
2023-02-21 14:31:48 +0800

    documentation for the Device 📝

--- core/src/backend/device.rs
+++ core/src/backend/device.rs
@@ -1,22 +1,108 @@
-use std::{sync::Arc, ffi::CStr};
+use std::{
+    sync::Arc,
+    ffi::CStr
+};
 
+/// Represents a CUDA or OpenCL device.
+/// This struct provides a simple way to retrieve information about a device
+/// that can be used for hardware accelerated computation.
+/// 
+/// This can be constructed by using `Device::devices` that returns an iterator for enumerating available devices.
+///
+/// # Examples
+///
+/// ```
+/// // Enumerate all devices available
+/// for device in Device::devices(BackendApi::All).unwrap().flatten() {
+///     if device.backend() == BackendApi::Cuda {
+///         let cuda_name = device.name().unwrap();
+///         assert_eq!(cuda_name, "GeForce RTX 2080 Ti");
+///     } else {
+///         let cl_name = device.name().unwrap();
+///         assert_eq!(cl_name, "AMD Radeon RX 5700 XT");
+///     }
+/// }
+/// ```
+///
+/// Note that the names of the devices in these examples are just placeholders, and will
+/// depend on the actual devices installed on the system.
 pub struct Device(pub(super) Arc<dyn DeviceInner>);
 
 impl Device {
+
+    /// Returns an iterator over all available CUDA or OpenCL devices.
+    ///
+    /// This function takes a `BackendApi` parameter indicating the desired backend API (either CUDA or OpenCL).
+    /// If the function returns `Result::Ok`, an iterator is returned alongside it that can be used to enumerate all devices
+    /// that are available for the specified backend API.
+    ///
+    /// # Errors
+    ///
+    /// Returns an `Result::Err(Error)` if the specified backend API is not available or if an error occurs while querying devices.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// // Enumerate all CUDA devices available
+    /// for device in Device::devices(BackendApi::Cuda).unwrap().flatten() {
+    ///     let name = device.name().unwrap();
+    ///     println!("Found CUDA device: {}", name);
+    /// }
+    ///
+    /// // Enumerate all OpenCL devices available
+    /// for device in Device::devices(BackendApi::OpenCl).unwrap().flatten() {
+    ///     let name = device.name().unwrap();
+    ///     println!("Found OpenCL device: {}", name);
+    /// }
+    /// ```
     pub fn devices(backend: super::BackendApi) -> crate::Result<super::QueryDeviceIter> {
         super::QueryDeviceIter::new(backend)
     }
 
+    /// Creates an `Engine` from the device.
+    /// # Returns
+    /// A `Result::Ok` that contains a `Engine` object if the engine creation was successful.
+    /// # Errors
+    ///
+    /// Returns an `Result::Err(Error)` if the underlying context was failed to be created.
     #[inline]
     pub fn create_engine(&self) -> crate::Result<super::Engine> {
         self.0.create_engine()
     }
 
+    /// Returns the backend API of the device.
+    ///
+    /// # Returns
+    ///
+    /// - `BackendApi::Cuda` if the device is a Nvidia CUDA device.
+    /// - `BackendApi::Cl` if the device is an OpenCL device.
+    /// # Examples
+    /// ```
+    ///  match device.backend() {
+    ///     BackendApi::Cuda => println!("This device is a Nvidia CUDA device."),
+    ///     BackendApi::Cl => println!("This device is an OpenCL device."),
+    ///  }
+    /// ```
     #[inline]
     pub fn backend(&self) -> super::BackendApi {
         self.0.backend()
     }
 
+    /// Returns the name of the device.
+    ///
+    /// # Returns
+    ///
+    /// The name of the device as a `String` if it is successfully retrieved.
+    ///
+    /// # Errors
+    ///
+    /// Returns an `Result::Err(Error)` if there was a problem retrieving the device name.
+    /// ```
+    /// match device.name() {
+    ///     Ok(name) => println!("The name of the device is {}.", name),
+    ///     Err(e) => println!("Error: {}", e),
+    /// }
+    /// ```
     #[inline]
     pub fn name(&self) -> crate::Result<String> {
         self.0.name()
