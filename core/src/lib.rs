143820f9c8901636bd013f3e52f3ca5a73b30083
neilcantorne <cantorn.neil@gmail.com>
2023-02-21 10:59:37 +0800

    include new add items to root module

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,4 +1,4 @@
-mod engine;
+mod backend;
 mod codegen;
 mod layer;
 mod layer_trainables;
@@ -11,8 +11,8 @@ mod activation_function;
 use error::ErrorVariants as Errors;
 pub use error::Error;
 pub use error::Result;
+pub use backend::{ Device, Engine, Backend };
 pub mod layers;
-pub use engine::Engine;
 pub use codegen::Value;
 pub use layer::Layer;
 pub use layer_trainables::LayerTrainables;
@@ -20,3 +20,5 @@ pub use dimension::Dimension;
 pub use tensor::Tensor;
 pub use activation_function::ActivationFunction;
 pub use element::{ Element, ChannelCount };
+
+mod tests;
