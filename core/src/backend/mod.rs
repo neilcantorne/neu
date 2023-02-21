09601e9dc75ee929d7819b9f3855c3f0c61c9294
neilcantorne <cantorn.neil@gmail.com>
2023-02-21 10:58:22 +0800

    implement root module of backend

---
+++ core/src/backend/mod.rs
@@ -0,0 +1,29 @@
+mod engine;
+mod device;
+mod query_device_iter;
+
+pub use engine::Engine;
+pub use device::Device;
+pub use query_device_iter::QueryDeviceIter;
+
+use device::{ CudaDevice, ClDevice };
+
+// Short enums
+pub enum Backend {
+    Cuda,
+    OpenCl,
+    All,
+}
+
+// Cuda initialization related code
+static CudaInit: bool = false;
+
+fn initialize_cuda() -> crate::Result<()>{
+    unsafe {
+        if !CudaInit && cuda_driver_sys::cuInit(0) != cuda_driver_sys::cudaError_enum::CUDA_SUCCESS {
+            return crate::Errors::FailedToInitializeCuda.into();
+        }
+
+        Ok(())
+    }
+}
