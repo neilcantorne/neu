af6cf93878e7893a9d095e59190e4541083e7d56
neilcantorne <cantorn.neil@gmail.com>
2023-02-22 21:44:07 +0800

    update root

--- bindgen/src/lib.rs
+++ bindgen/src/lib.rs
@@ -1,9 +1,14 @@
 use proc_macro::TokenStream;
+use proc_macro2::Ident;
+use syn::parse;
 
 mod dl_link_item;
-mod dl_link_attr;
 
 #[proc_macro_attribute]
-pub fn dl_link(_attr: TokenStream, _item: TokenStream) -> TokenStream {
-    todo!()
+pub fn dl_link(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let name: Ident = parse(attr).unwrap();
+    let dl_link_item: dl_link_item::DlLinkItem = parse(item).unwrap();
+    
+    //panic!("{}", dl_link_item.generate(name));
+    dl_link_item.generate(name).into()
 }
