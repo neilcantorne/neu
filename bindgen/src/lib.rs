d908b44c75e6dab1049953aaab41bfd8e9c9b9be
neilcantorne <cantorn.neil@gmail.com>
2023-02-24 01:40:45 +0800

    implement derive_handle

--- bindgen/src/lib.rs
+++ bindgen/src/lib.rs
@@ -12,3 +12,19 @@ pub fn dl_link(attr: TokenStream, item: TokenStream) -> TokenStream {
     //panic!("{}", dl_link_item.generate(name));
     dl_link_item.generate(name).into()
 }
+
+#[proc_macro_derive(Handle)]
+pub fn derive_handle(item: TokenStream) -> TokenStream {
+    let item_struct: syn::ItemStruct = parse(item).unwrap();
+    let ident = item_struct.ident;
+    
+    quote::quote!(
+        impl Handle for #ident {
+            const NULL: Self = Self(0);
+
+            fn is_null(&self) -> bool {
+                self.0 == 0
+            }
+        }
+    ).into()
+}
